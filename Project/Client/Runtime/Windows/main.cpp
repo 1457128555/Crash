a5a59ca48c66286e314da428154710f63f097a73
chensheng <1457128555@qq.com>
2025-06-16 01:27:36 +0800

    add

--- Project/Client/Runtime/Windows/main.cpp
+++ Project/Client/Runtime/Windows/main.cpp
@@ -7,7 +7,7 @@
 
 #include "CrashRenderProtocol.hpp"
 
-#include "HelloTriangle01.h"
+#include "HelloBlending04.h"
 
 using namespace Crash;
 
@@ -97,7 +97,7 @@ int main()
     config.swapBuffer           = []() { glfwSwapBuffers(sWindow); };
 
     new Crash::Engine(config);
-    sScene = new HelloTriangle01();
+    sScene = new HelloBlending04();
     Crash::Engine::Instance()->setScene(sScene);
 
     auto start = std::chrono::steady_clock::now();
